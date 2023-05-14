9ca18d4f20d464b78cebb8667ba1c593d6812768
josephleee <jungsup@kakao.com>
2023-05-15 01:17:22 +0900

    modify get_product_list to get all products in a page

--- crawler_modules/c_parser/parser_naver_store.py
+++ crawler_modules/c_parser/parser_naver_store.py
@@ -29,37 +29,55 @@ class NaverShoppingCrawler:
         # df = pd.DataFrame(data)
         # df.to_csv('naver_shopping_data.csv', index=False)
 
-    def get_product_list(self, keyword, n=50):
-        # url = "/api/search/all?eq=&iq=&pagingIndex=1&pagingSize=40&productSet=total&query=%EB%82%A8%EC%84%B1%20%EC%8A%A4%EB%8B%88%EC%BB%A4%EC%A6%88&sort=price_asc&viewType=list&xq="
+    # def get_product_list(self, keyword, n=50):
+    #     # url = "/api/search/all?eq=&iq=&pagingIndex=1&pagingSize=40&productSet=total&query=%EB%82%A8%EC%84%B1%20%EC%8A%A4%EB%8B%88%EC%BB%A4%EC%A6%88&sort=price_asc&viewType=list&xq="
 
-        url = f'https://search.shopping.naver.com/search/all?query={keyword}'
+    #     url = f'https://search.shopping.naver.com/search/all?query={keyword}'
+    #     headers = {
+    #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    #     response = requests.get(url, headers=headers)
+    #     soup = BeautifulSoup(response.content, 'html.parser')
+    #     data_list = []
+    #     for i in range(1, n+1):
+    #         data = {
+    #             "name": self.extract_product_data(soup, i, "name"),
+    #             "is_ad": self.extract_product_data(soup, i, "is_ad"),
+    #             "price": self.extract_product_data(soup, i, "price"),
+    #             "shipping_fee": self.extract_product_data(soup, i, "shipping_fee"),
+    #             "category": self.extract_product_data(soup, i, "category"),
+    #             "ad_event_comment": self.extract_product_data(soup, i, "ad_event_comment"),
+    #             "reviews": self.extract_product_data(soup, i, "reviews"),
+    #             "register_date": self.extract_product_data(soup, i, "register_date"),
+    #             "zzims": self.extract_product_data(soup, i, "zzims"),
+    #             "seller_name": self.extract_product_data(soup, i, "seller_name"),
+    #             "grade": self.extract_product_data(soup, i, "grade"),
+    #             "reward_naver_points": self.extract_product_data(soup, i, "reward_naver_points"),
+    #             "url": self.extract_product_data(soup, i, "url"),
+    #             "is_smartstore": self.extract_product_data(soup, i, "is_smartstore"),
+    #         }
+    #         if data["name"] == None and data["price"] == None:
+    #             break
+    #         data_list.append(data)
+
+    #     return data_list
+
+    def get_product_list(self, keyword, n=50):
+        cat_id = ""
+        frm = "NVSHATC"
+        url = f'https://search.shopping.naver.com/search/all?query={keyword}&cat_id={cat_id}&frm={frm}'
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+            "accept": 'application/json, text/plain, */*',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'ko-KR,ko;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-US;q=0.6,en;q=0.5',
+            "content-type": 'application/json;charset=UTF-8',
+            "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        }
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        data_list = []
-        for i in range(1, n+1):
-            data = {
-                "name": self.extract_product_data(soup, i, "name"),
-                "is_ad": self.extract_product_data(soup, i, "is_ad"),
-                "price": self.extract_product_data(soup, i, "price"),
-                "shipping_fee": self.extract_product_data(soup, i, "shipping_fee"),
-                "category": self.extract_product_data(soup, i, "category"),
-                "ad_event_comment": self.extract_product_data(soup, i, "ad_event_comment"),
-                "reviews": self.extract_product_data(soup, i, "reviews"),
-                "register_date": self.extract_product_data(soup, i, "register_date"),
-                "zzims": self.extract_product_data(soup, i, "zzims"),
-                "seller_name": self.extract_product_data(soup, i, "seller_name"),
-                "grade": self.extract_product_data(soup, i, "grade"),
-                "reward_naver_points": self.extract_product_data(soup, i, "reward_naver_points"),
-                "url": self.extract_product_data(soup, i, "url"),
-                "is_smartstore": self.extract_product_data(soup, i, "is_smartstore"),
-            }
-            if data["name"] == None and data["price"] == None:
-                break
-            data_list.append(data)
-
-        return data_list
+        scripts = soup.find("script", {"id": "__NEXT_DATA__"})
+        scripts_data = json.loads(scripts.text.replace(';','').strip())
+        products = self.extract_product_data(scripts_data)
+        return products
 
     def get_store_review(self, request_ids):
         '''
@@ -91,7 +109,7 @@ class NaverShoppingCrawler:
             product_review = self.extract_store_review_data(self, response)
             data_list.append(product_review)
             
-        return 
+        return data_list
 
     def get_integrated_review():
         pass
@@ -112,48 +130,76 @@ class NaverShoppingCrawler:
             data_list.append(data)
         return data_list
 
-    def extract_product_data(self, soup, i, name):
-        '''
-        html 태그에서 가져오는 방식으로는 상위 5개밖에 가져오지 못함
-        TODO: 
-            try request api to get all results
-        '''
-        selector_dict = {
-            "name": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_title__VfX3c > a",
-            "is_ad": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > button",
-            "price": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > strong > span > span.price_price__LEGN7 > span",
-            "shipping_fee": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > strong > span > span.price_delivery__yw_We",
-            "category": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_depth__SbZWF > span",
-            "ad_event_comment": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_desc__3kwkT > div > a",
-            "reviews": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > a > em",
-            "register_date": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > span:nth-child(2)",
-            "zzims": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > span:nth-child(3) > a > span > em",
-            "seller_name": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > div.basicList_mall_title__FDXX5 > a.basicList_mall__BC5Xu.linkAnchor",
-            "grade": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > div.basicList_mall_grade__1hPzs > span",
-            "reward_naver_points": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > ul > li:nth-child(1) > span.n_npay_info__TqvjM > span",
-            "url": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_title__VfX3c > a",
-            "is_smartstore": f""
-        }
-        selector = selector_dict.get(name)
-        if not selector:
-            return
-        result = soup.select(selector)
-        if name[:3] == "is_":
-            return True if result else False
-        if not result:
-            return None
+    # def extract_product_data(self, soup, i, name):
+    #     '''
+    #     html 태그에서 가져오는 방식으로는 상위 5개밖에 가져오지 못함
+    #     TODO: 
+    #         try request api to get all results
+    #     '''
+    #     selector_dict = {
+    #         "name": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_title__VfX3c > a",
+    #         "is_ad": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > button",
+    #         "price": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > strong > span > span.price_price__LEGN7 > span",
+    #         "shipping_fee": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_price_area__K7DDT > strong > span > span.price_delivery__yw_We",
+    #         "category": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_depth__SbZWF > span",
+    #         "ad_event_comment": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_desc__3kwkT > div > a",
+    #         "reviews": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > a > em",
+    #         "register_date": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > span:nth-child(2)",
+    #         "zzims": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_etc_box__5lkgg > span:nth-child(3) > a > span > em",
+    #         "seller_name": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > div.basicList_mall_title__FDXX5 > a.basicList_mall__BC5Xu.linkAnchor",
+    #         "grade": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > div.basicList_mall_grade__1hPzs > span",
+    #         "reward_naver_points": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_mall_area__faH62 > ul > li:nth-child(1) > span.n_npay_info__TqvjM > span",
+    #         "url": f"#content > div.style_content__xWg5l > div.basicList_list_basis__uNBZx > div > div:nth-child({i}) > div > div > div.basicList_info_area__TWvzp > div.basicList_title__VfX3c > a",
+    #         "is_smartstore": f""
+    #     }
+    #     selector = selector_dict.get(name)
+    #     if not selector:
+    #         return
+    #     result = soup.select(selector)
+    #     if name[:3] == "is_":
+    #         return True if result else False
+    #     if not result:
+    #         return None
 
-        if name == "url":
-            return result[0].attrs.get("href")
-        elif name == "register_date":
-            return result[0].text.replace('등록일 ', '')
-        elif name == "reward_naver_points":
-            return result[0].text.replace('포인트 ', '')
+    #     if name == "url":
+    #         return result[0].attrs.get("href")
+    #     elif name == "register_date":
+    #         return result[0].text.replace('등록일 ', '')
+    #     elif name == "reward_naver_points":
+    #         return result[0].text.replace('포인트 ', '')
 
-        if len(result) > 1:
-            return [r.text for r in result]
-        else:
-            return result[0].text
+    #     if len(result) > 1:
+    #         return [r.text for r in result]
+    #     else:
+    #         return result[0].text
+
+    def extract_product_data(self, response):
+        product_list = []
+        products = response["props"]["pageProps"]["initialState"]["products"]["list"]
+        for p in products:
+            product_info = {
+                "productTitle" : p.get("productTitle"), # 상품명
+                "adProductInfoEnabled" : p.get("adProductInfoEnabled"), # 광고여부
+                "price" : p.get("price"), # 가격
+                "deliveryFeeContent" : p.get("deliveryFeeContent"), # 배송비
+                "category" : [p.get("category1Name"), p.get("category2Name"), p.get("category3Name")],
+                "adAdditionalDescription" : p.get("adAdditionalDescription"), # 사용자문구
+                "reviewCount" : p.get("reviewCount"), # 리뷰수
+                "openDate" : p.get("openDate"), # 등록일
+                "mallGrade" : p["mallInfoCache"].get("mallGrade") if p.get("mallInfoCache") else None, # 몰 등급
+                "naverPayAdAccumulatedDisplayValue" : p.get("naverPayAdAccumulatedDisplayValue"), # 포인트 원
+                "adcrUrl" : p.get("adcrUrl"), # url
+                "mallNameOrg" : p.get("mallNameOrg"), # 몰 회사명
+                "isBrandStore" : p.get("isBrandStore"), # 스마트스토어 여부 0: 기타, 1: 스마트스토어, 3: 스토어 모음
+                "mall" : {
+                    "mallNo": p.get("mallNo"),
+                    "mallId": p.get("mallId"),
+                    "mallName": p.get("mallName"),
+                } # 셀러이름
+                # TODO: 찜하기 -> 특이
+            }
+            product_list.append(product_info)
+        return product_list
 
     def extract_store_review_data(self, response):
         
